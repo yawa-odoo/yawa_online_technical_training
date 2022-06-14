cb42a82eb56cb8e75cb7c2ca88ba065888aedbe5
Yanchao Wang <yawa@odoo.com>
2022-06-14 16:00:52 +0000

    [ADD] Academy Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary' : """Academy app to manage Training""".
+    'description': """
+        Academy Module to Manage Training:
+    """,
+    'author': 'Odoo',
+    'website': 'www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'].
+    'data':[
+        
+    ],
+    'demo': [
+        
+    ]
+}
