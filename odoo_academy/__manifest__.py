57c7c7649103d0c514ec058036c1aba707fa7409
Yanchao Wang <yawa@odoo.com>
2022-06-15 10:04:01 -0400

    Added menuitem

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -15,6 +15,7 @@
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
+        'views/academy_menuitems.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
