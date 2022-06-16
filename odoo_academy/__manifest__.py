41afb01c6d74f6156c78dcc0ad0f2fe077375a9e
Yanchao Wang <yawa@odoo.com>
2022-06-16 09:50:34 -0400

    'Views' tutorial code

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -2,6 +2,7 @@
 
 {
     'name': 'Odoo Academy',
+    'application': True,
     'summary': """Academy app to manage Training""",
     'description': """
         Academy Module to Manage Training:
@@ -16,6 +17,7 @@
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
+        'views/course_views.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
