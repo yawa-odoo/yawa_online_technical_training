58fec16dd367997f3414551d971c00b06d91f74b
Yanchao Wang <yawa@odoo.com>
2022-06-17 14:57:45 -0400

    Inheritance demo code

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -12,13 +12,14 @@
     'website': 'www.odoo.com',
     'category': 'Training',
     'version': '0.1',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': [
         'security/academy_security.xml',
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
         'views/session_views.xml',
+        'views/sale_views_inherit.xml',
     ],
     'demo': [
         'demo/academy_demo.xml',
