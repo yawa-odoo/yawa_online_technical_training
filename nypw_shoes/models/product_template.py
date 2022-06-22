1ff087f7f666765f09c44dd707cf4113e16c81a5
yawa <yawa@odoo.com>
2022-06-22 12:39:50 -0400

    [IMP] Simplified the way to set the read_only attribute

--- nypw_shoes/models/product_template.py
+++ nypw_shoes/models/product_template.py
@@ -11,7 +11,6 @@ class ProductTemplate(models.Model):
         string='Pair per case', default=None, required=True)
     price_per_pair = fields.Monetary(
         string='Price per pair', default=None, required=True)
-    fields_empty = fields.Boolean(string='Whether one of pair_per_case and price_per_pair is empty', default=False)
     sales_price = fields.Monetary(string='Sales Price', store=True)
 
     @api.onchange('pair_per_case', 'price_per_pair')
@@ -20,5 +19,4 @@ class ProductTemplate(models.Model):
             raise UserError('Pair per case cannot be negative')
         if self.price_per_pair < 0.00:
             raise UserError('Price per pair cannot be negative')
-        self.fields_empty = not self.pair_per_case and not self.price_per_pair
         self.sales_price = self.pair_per_case * self.price_per_pair
